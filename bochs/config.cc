fc3afa012a4f740612b1f41cad41d02a0953781f
Christopher <cvella@microsoft.com>
2025-11-20 12:40:20 +1100

    upd

--- bochs/config.cc
+++ bochs/config.cc
@@ -2836,7 +2836,7 @@ static int parse_line_formatted(const char *context, int num_params, char *param
         SIM->get_param_string(BXPN_ROM_PATH)->set(&params[i][5]);
       } else  if (!strncmp(params[i], "address=", 8)) {
         if ((params[i][8] == '0') && (params[i][9] == 'x'))
-          SIM->get_param_num(BXPN_ROM_ADDRESS)->set(strtoul(&params[i][8], NULL, 16));
+          SIM->get_param_num(BXPN_ROM_ADDRESS)->set(strtoull(&params[i][8], NULL, 16));
         else
           SIM->get_param_num(BXPN_ROM_ADDRESS)->set(strtoul(&params[i][8], NULL, 10));
       } else  if (!strncmp(params[i], "flash_data=", 11)) {
